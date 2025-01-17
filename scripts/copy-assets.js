22ff3d7aec9023e592892a9f61d638858d172590
Maksim Chertkov <maxch@unl.solutions>
2025-01-17 15:18:07 +0100

    #5 copy assets for dist build

---
+++ scripts/copy-assets.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+
+// SPDX-FileCopyrightText: 2025 NOI Techpark <digital@noi.bz.it>
+//
+// SPDX-License-Identifier: CC0-1.0
+
+/*
+ stencil 'dist-custom-elements' does not support assets copying, so we do it manually
+ */
+const fs = require('fs');
+const path = require('path');
+
+const assetsSrc = __dirname + '/../assets';
+const assetsDst = __dirname + '/../dist/components/assets';
+
+fs.cpSync(assetsSrc, assetsDst, {recursive: true});
+
+console.log('[copy-assets] done');
+
--- scripts/update-manifest-files.js
+++ scripts/update-manifest-files.js
@@ -12,22 +12,36 @@ const fileContent = fs.readFileSync(manifestFile, 'utf8');
 const fileData = JSON.parse(fileContent);
 
 //
-const folderPath = path.normalize(path.join(__dirname, '../', fileData.dist.basePath));
-console.log('[manifest] Add files from:', folderPath);
-const dirContent = fs.readdirSync(folderPath);
-const jsFiles = dirContent.filter(f => f.endsWith('.js'));
+const distPath = path.normalize(path.join(__dirname, '../', fileData.dist.basePath));
+console.log('[manifest] Add files from:', distPath);
+const dirContent = fs.readdirSync(distPath, {recursive: true, withFileTypes: true});
+
+// determine files to include
+const jsFiles = dirContent.filter(f => {
+  if (!f.isFile()) {
+    return false;
+  }
+  return !f.name.endsWith('.js.map');
+});
 
 if (!jsFiles.length) {
   throw new Error('No files found');
 }
 
+// get relative path
+const relativePath = jsFiles.map(dirent => {
+  const fullPath = (dirent.parentPath || dirent.path) + '/' + dirent.name;
+  return path.relative(distPath, fullPath);
+})
+
+// print files to copy
 if (process.argv.includes('-v') || process.argv.includes('--verbose')) {
-  console.log(jsFiles);
+  console.log(relativePath);
 }
 
 //
-fileData.dist.files = jsFiles;
+fileData.dist.files = relativePath;
 fs.writeFileSync(manifestFile, JSON.stringify(fileData, null, 2));
 
-console.log('[manifest] %s files found', jsFiles.length);
+console.log('[manifest] %s files found', relativePath.length);
 
