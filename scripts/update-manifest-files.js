835b8f1895bb82f0124d71b80da84cb8de8dfa6f
Maksim Chertkov <maxch@unl.solutions>
2025-01-24 01:51:52 +0100

    #5 (build) copy necessary files only

--- scripts/update-manifest-files.js
+++ scripts/update-manifest-files.js
@@ -4,6 +4,7 @@
 //
 // SPDX-License-Identifier: CC0-1.0
 
+// node >=20 is required to run it correctly
 const fs = require('fs');
 const path = require('path');
 
@@ -11,17 +12,50 @@ const manifestFile = __dirname + '/../wcs-manifest.json';
 const fileContent = fs.readFileSync(manifestFile, 'utf8');
 const fileData = JSON.parse(fileContent);
 
+const isVerbose = process.argv.includes('-v') || process.argv.includes('--verbose');
+
 //
 const distPath = path.normalize(path.join(__dirname, '../', fileData.dist.basePath));
 console.log('[manifest] Add files from:', distPath);
 const dirContent = fs.readdirSync(distPath, {recursive: true, withFileTypes: true});
+
+const entryPoints = [
+  'noi-road-webcam.js',
+];
 
 // determine files to include
 const jsFiles = dirContent.filter(f => {
   if (!f.isFile()) {
     return false;
   }
-  return !f.name.endsWith('.js.map');
+
+  // assets
+  if (f.name.endsWith('.json')) {
+    return true;
+  }
+
+  // entry points
+  if (entryPoints.includes(f.name)) {
+    return true;
+  }
+
+  // skip sourcemaps
+  if (f.name.endsWith('.js.map')) {
+    if (isVerbose) {
+      console.log('skipped:', (f.parentPath || f.path) + '/' + f.name);
+    }
+    return false;
+  }
+
+  // component partials
+  if (f.name.startsWith('p-')) {
+    return true;
+  }
+
+  if (isVerbose) {
+    console.log('skipped:', (f.parentPath || f.path) + '/' + f.name);
+  }
+  return false;
 });
 
 if (!jsFiles.length) {
